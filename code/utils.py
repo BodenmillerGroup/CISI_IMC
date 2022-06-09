14658be2822efdde1b2aa71efd9033b406e4c74a
leonorss <leonor.schubert@gmail.com>
2022-06-09 17:22:36 +0200

    Added a fnc which extracts important information from a typical cell.csv and tested it.

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,11 @@
 # Import libraries
 import numpy as np
 import spams
+import os
+from pathlib import Path
+import pandas as pd
+from re import search
+from analyze_predictions import *
 
 
 '''
@@ -9,32 +14,91 @@ Helper file
 '''
 
 
+'''
+New functions
+'''
+
+# Function that checks that file_path is an existing file and has a certain extension
+def check_file(file_path, extension):
+    file = Path(file_path)
+    if file.exists() and file.is_file() and file.suffix in extension:
+        return True
+    else:
+        return False
+
+
+# Function that reads in a cell.csv type file and prepares it for decomposition
+# training (selects only meanIntensity, ImageNumber and ObjectNumber columns and
+# determines protein names)
+def read_input(X_input, panel_input):
+    # Check if input is a valid csv file
+    if check_file(X_input, ['.csv']) and check_file(panel_input, ['.csv']):
+        # Read intensities form .csv file
+        X = np.genfromtxt(X_input, delimiter=',', skip_header=1)
+        # Read in header names
+        X_header = pd.read_csv(X_input, nrows=0).columns.tolist()
+        # Get list of mean intensity columns and Image/Object number columns
+        selected_columns = list(map(lambda col:
+            bool(search('(Intensity_MeanIntensity_FullStackFiltered_c|ImageNumber|ObjectNumber)', col)),
+            X_header))
+        # Filter for above selected columns
+        X = X[:, selected_columns]
+        # Remove everything before channel numbers in X_header
+        X_header = [x.replace("Intensity_MeanIntensity_FullStackFiltered_c", "")
+                    for x in X_header]
+
+        # Read in panel data
+        panel = pd.read_csv(panel_input)
+        proteins = [panel.loc[panel['channel'] == int(i), 'Target'].values[0]
+                    for i in X_header if i not in ['ImageNumber', 'ObjectNumber']]
+
+    else:
+        # If it is none of the above input types an error is
+        raise ValueError('''No valid input for X was given.
+              The input for X needs to be a .csv, .npy or numpy array with
+              dimensions: proteins x cells (/pixels?).''')
+
+    return X, X_header, proteins
+
+
+'''
+Old functions (from original publication)
+'''
+
 # Fixing U, approximating W
-def sparse_decode(Y, D, k, numThreads, worstFit=1., mink=0):
-    while k > mink:
-        W = spams.omp(np.asfortranarray(Y), np.asfortranarray(D), L=k, numThreads=numThreads)
+def sparse_decode(Y, D, k, numThreads, method='omp', worstFit=1., mink=0, nonneg=False):
+    if method == 'omp':
+        while k > mink:
+            W = spams.omp(np.asfortranarray(Y), np.asfortranarray(D), L=k,
+                 numThreads=numThreads)
+            W = np.asarray(W.todense())
+            fit = 1 - np.linalg.norm(Y - D.dot(W))**2 / np.linalg.norm(Y)**2
+            if fit < worstFit:
+                break
+            else:
+                k -= 1
+    elif method == 'lasso':
+        Ynorm = np.linalg.norm(Y)**2 / Y.shape[1]
+        W = spams.lasso(np.asfortranarray(Y), np.asfortranarray(D), lambda1=k*Ynorm,
+                  mode=1, numThreads=numThreads, pos=nonneg)
         W = np.asarray(W.todense())
-        fit = 1 - np.linalg.norm(Y - D.dot(W))**2 / np.linalg.norm(Y)**2
-        if fit < worstFit:
-            break
-        else:
-            k -= 1
+
     return W
 
 
 # Simulate random composite observations Y from X
 def get_observations(X0, Phi, snr=5, return_noise=False):
-	noise = np.array([np.random.randn(X0.shape[1]) for _ in range(X0.shape[0])])
-	noise *= np.linalg.norm(X0)/np.linalg.norm(noise)/snr
-	if return_noise:
-		return Phi.dot(X0 + noise), noise
-	else:
-		return Phi.dot(X0 + noise)
+    noise = np.array([np.random.randn(X0.shape[1]) for _ in range(X0.shape[0])])
+    noise *= np.linalg.norm(X0)/np.linalg.norm(noise)/snr
+    if return_noise:
+        return Phi.dot(X0 + noise), noise
+    else:
+        return Phi.dot(X0 + noise)
 
 
 # Compare X to predicted X by correlations and distances between them
 def compare_results(A, B):
-	results = list(correlations(A, B, 0))[:-1]
-	results += list(compare_distances(A, B))
-	results += list(compare_distances(A.T, B.T))
-	return results
+    results = list(correlations(A, B, 0))[:-1]
+    results += list(compare_distances(A, B))
+    results += list(compare_distances(A.T, B.T))
+    return results
---
+++ tests/test_utils.py
@@ -0,0 +1,62 @@
+# Import system libraries to configure code directory as module
+from os.path import dirname, abspath, join
+import sys
+
+# Find code directory relative to our directory
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..', 'code'))
+# Add code directory to systems paths
+sys.path.append(CODE_DIR)
+
+# Import libraries
+import unittest
+import numpy as np
+import utils
+
+
+# Test inputs to compute_dictionary
+class TestInputX(unittest.TestCase):
+
+    def test_csv_validity(self):
+        t = utils.check_file('data/cell.csv', [".csv"])
+        self.assertTrue(t)
+
+    def test_npy_validity(self):
+        t = utils.check_file('data/training_data_sce.npy', [".npy"])
+        self.assertTrue(t)
+
+    def test_X_input(self):
+        X, X_header, proteins = utils.read_input('data/cell.csv', 'data/TH152_panel.csv')
+        self.assertIsNone(np.testing.assert_array_equal(X[:5, 2],
+                                                        np.array([0.000158776,
+                                                                  0.000232407,
+                                                                  0.000186816,
+                                                                  0.000181502,
+                                                                  0.000392305])))
+
+    def test_X_header(self):
+        t = ['ImageNumber', 'ObjectNumber', '1', '10', '11', '12', '13', '14', '15',
+             '16', '17', '18', '19', '2', '20', '21', '22', '23', '24', '25', '26',
+             '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', '37',
+             '38', '39', '4', '40', '41', '42', '43', '5', '6', '7', '8', '9']
+        X, X_header, proteins = utils.read_input('data/cell.csv', 'data/TH152_panel.csv')
+        self.assertListEqual(X_header, t)
+
+    def test_proteins(self):
+        t = ['Histone H3', 'CD45RA', 'CD20', 'CD11b', 'CD56 ', 'GranzymeB', 'CD3',
+             'DC-LAMP', 'CD11c', 'PD-1', 'GITR', 'SMA', 'MMP9', 'CD14', 'PD-L1',
+             'TCF1/TCF7', 'CD45RO', 'FOXP3', 'ICOS', 'Ki-67', 'CD8a', 'Tim-3',
+             'E-Cad/P-Cad', 'IRF4', 'VISTA', 'CD1c', 'CD4', 'CD31', 'CXCL12',
+             'CCL21', 'panCK', 'CXCL13', 'Ir191', 'CCR7', 'Ir193', 'Vimentin',
+             'CD15', 'MPO', 'CD38', 'HLA-DR', 'CD27', 'CD303', 'CD68']
+        X, X_header, proteins = utils.read_input('data/cell.csv', 'data/TH152_panel.csv')
+        self.assertListEqual(proteins, t)
+
+    def test_input_error(self):
+        with self.assertRaises(ValueError):
+            utils.read_input('dat', 'data/TH152_panel.csv')
+
+
+## Main
+if __name__ == '__main__':
+    unittest.main()
