b4a9067215ded6e3322cc96c7afc711a1cb3b11a
leonorss <leonor.schubert@gmail.com>
2022-07-20 15:06:20 +0200

    Added different options for normalizations before smaf to compute_dictionary and adjusted it to ran with anndata objects. Tested for executability. Also, added some more comments.

--- code/compute_dictionary.py
+++ code/compute_dictionary.py
@@ -8,6 +8,8 @@ from scipy.spatial import distance
 from pathlib import Path
 from utils import sparse_decode
 import os
+from scipy.stats import zscore
+import pandas as pd
 
 
 '''
@@ -16,12 +18,29 @@ Sparse Module Activity Factorization (SMAF)
 
 Find X = UW with special constraints:
 inputs:
-    X: proteins x cells (/pixels?) -> numpy array
+    X_input: anndata object containing numpy array X (proteins x cells/pixels)
     d: the number of features (columns) in the dictionary U
     lda1: in mode 1 (recommended) the number of nonzeros per column in W
     lda2: an error threshold - when optimizing over U we will search for the
           sparsest fit while tolerating at most this error
+    mode: Mode in which spams lasso fnc. regularizes U and W (constraint of min.)
+          (default: mode=1, Note: mode=2 seems to produce empty matrices in some cases,
+          maybe constraint are to stringthend?)
+          Find columns alpha for each column x in X:
+          Mode=1
+            min_{alpha} + ||alpha||_1 s.t. ||x-W*alpha||_2^2 <= lambda1
+          Mode=2
+            min_{alpha} + 0.5||x-W*alpha||_2^2 + lambda1||alpha||_1 +0.5 lambda2||alpha||_2^2
     THREADS: # Number of threads used (default=4)
+    doprint: Print correlations between predicted X and real X and some additional info
+    normalization: How data is normalized before running smaf (default: paper_norm)
+                   Options: paper_norm (normalization used in paper, protein-wise),
+                   min_max_norm (accross whole matrix), min_max_norm_channelwise
+                   (protein-wise), zscore_norm (accross whole image) or none
+    layer: Name of layer in anndata object to be used as X (default: None, =anndata.X)
+    others: All other parameters belong to the fnc. call of lasso from spams and
+            further information is available on the respective website
+
 outputs:
     U: a dictionary of gene modules (proteins x modules)
     W: the module activity levels in each cell (/pixel?) of training data
@@ -36,11 +55,36 @@ Example:
 def smaf(X_input, d, lda1, lda2, maxItr=10, UW=None, posW=False, posU=True,
          use_chol=False, module_lower=1, activity_lower=1, donorm=False,
          mode=1, mink=0, U0=[], U0_delta=0.1, doprint=False, THREADS=4,
-         outpath=None):
+         outpath=None, normalization='paper_norm', layer=None):
 
-    # Normalize X? (otherwise spams.nmf underneath will estimate U to only contain
-    # 0s and smaf won't work)
-    X = (X_input.T / np.linalg.norm(X_input, axis=1)).T
+    # Select layer of anndata object that should be used in SMAF and transpose it
+    # to proteins x cells/channels
+    if layer is not None:
+        X_mat = (X_input.layers[layer]).T
+    else:
+        X_mat = (X_input.X).T
+
+    # Extract data matrix X from anndata object and apply selected normalization
+    match normalization:
+        case 'paper_norm':
+            # Normalize X? (otherwise spams.nmf underneath will estimate U to only contain
+            # 0s and smaf won't work)
+            # Normalizaiton: Every row in X_input is divided by the corresponding vector
+            # element in the rowwise norm (proteins are normalized accross all cells/pixels)
+            X = (X_mat.T / np.linalg.norm(X_mat, axis=1)).T
+        case 'min_max_norm':
+            X = (X_mat-X_mat.min()) / (X_mat.max()-X_mat.min())
+        case 'min_max_norm_channelwise':
+            X = (X_mat-X_mat.min(axis=1, keepdims=True)) / (X_mat.max(axis=1, keepdims=True)-                                                            X_mat.min(axis=1, keepdims=True))
+        case 'zscore_norm':
+            X = zscore(X_mat)
+        case 'none':
+            X = X_mat
+        case _:
+            # In case no valid normalization is given, an error is thrown
+            raise ValueError('The normalization {0} used by smaf is not valid.\
+                            Please use one of the following: paper_norm, min_max_norm,\
+                            min_max_norm_channelwise, zscore_norm or none.'.format(normalization))
 
     # use Cholesky when we expect a very sparse result
     # this tends to happen more on the full vs subsampled matrices
@@ -77,7 +121,7 @@ def smaf(X_input, d, lda1, lda2, maxItr=10, UW=None, posW=False, posU=True,
                 U = np.asarray(U.todense()).T
         # Default: False
         if donorm:
-            U = U / np.linalg.norm(U,axis=0)
+            U = U / np.linalg.norm(U, axis=0)
             U[np.isnan(U)] = 0
         # Default mode=1: estimate W
         if mode == 1:
@@ -114,6 +158,7 @@ def smaf(X_input, d, lda1, lda2, maxItr=10, UW=None, posW=False, posU=True,
         path.mkdir(parents=True, exist_ok=True)
         # Save U
         np.save(os.path.join(path, 'gene_modules.npy'), U)
-        np.savetxt(os.path.join(path, 'gene_modules.csv'), U, delimiter=',')
+        pd.DataFrame(U, columns=list(range(1, U.shape[1]+1)),
+                     index=X_input.var_names).to_csv(os.path.join(path, 'gene_modules.csv'))
 
     return U, W
