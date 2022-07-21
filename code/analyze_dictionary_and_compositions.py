211c04dca97e78b4f49a0006bc85f6bdde0636b8
leonorss <leonor.schubert@gmail.com>
2022-07-21 10:50:18 +0200

    Adapted analyze_dictionary_and_compositions to anndata objects and added a return statement of the analisys in a pandasa data frame.

--- code/analyze_dictionary_and_compositions.py
+++ code/analyze_dictionary_and_compositions.py
@@ -7,6 +7,7 @@ from utils import sparse_decode, compare_results, get_observations
 from pathlib import Path
 import sys
 import os
+import pandas as pd
 
 
 '''
@@ -15,7 +16,8 @@ Analyze performance of computed U and A
 
 For given X, U and A, simulate composite results y and analyze performance:
 inputs:
-    X: proteins x cells (/pixels?) -> .csv / .npy / numpy array
+    X: anndata object containing numpy array X (proteins x cells/pixels)
+       with proteins names as X.var_names
     U: a dictionary of gene modules (proteins x modules)
     Phi: list containing (best) composition matrices (composite channels (measurements) x proteins,
          binary)
@@ -25,10 +27,19 @@ inputs:
              compositions_A folder
 
 outputs:
+    results_df: All performance analyis in one pandas df
     simulation_results.txt: Performance analysis
 '''
 
-def analyze_U_and_A(X_test, U, Phi, outpath, lasso_sparsity=0.2, THREADS=20):
+def analyze_U_and_A(X_input, U, Phi, outpath, lasso_sparsity=0.2, THREADS=20,
+                    layer=None):
+    # Select layer of anndata object that should be used and transpose it
+    # to proteins x cells/channels
+    if layer is not None:
+        X_test = (X_input.layers[layer]).T
+    else:
+        X_test = (X_input.X).T
+
     # Empirical observation: using a sparsity constraint that is softer than
     # that used during training slightly improves results
     sparsity = lasso_sparsity / 10
@@ -44,10 +55,15 @@ def analyze_U_and_A(X_test, U, Phi, outpath, lasso_sparsity=0.2, THREADS=20):
     path.mkdir(parents=True, exist_ok=True)
     # Write output to file
     f2 = open(os.path.join(path, 'simulation_results.txt'), 'w')
-    f2.write('\t'.join(['version', 'Overall pearson', 'Overall spearman', 'Gene average',
-                     'Sample average', 'Sample dist pearson', 'Sample dist spearman',
-                     'Gene dist pearson', 'Gene dist spearman',
-                     'Matrix coherence (90th ptile)']) + '\n')
+    colnames = ['version', 'Overall pearson', 'Overall spearman', 'Gene average',
+                'Sample average', 'Sample dist pearson', 'Sample dist spearman',
+                'Gene dist pearson', 'Gene dist spearman',
+                'Matrix coherence (90th ptile)']
+    f2.write('\t'.join(colnames) + '\n')
+
+    # Create empty pandas df to return all analysis
+    results_df = pd.DataFrame(columns=colnames)
+
     for i in xs:
         phi = Phi[i]
         y = get_observations(X_test, phi, snr=5)
@@ -56,4 +72,9 @@ def analyze_U_and_A(X_test, U, Phi, outpath, lasso_sparsity=0.2, THREADS=20):
         results = compare_results(X_test, x2)
         f2.write('\t'.join([str(x) for x in [i]+results+[d_gene[i]]]) + '\n')
         print(d_gene[i], compare_results(X_test, x2))
+
+        # Add results to pandas df
+        results_df.loc[len(results_df)] = [i]+results+[d_gene[i]]
     f2.close()
+
+    return results_df
--- tests/test_analyze_dictionary_and_compositions.py
+++ tests/test_analyze_dictionary_and_compositions.py
@@ -14,18 +14,19 @@ import numpy as np
 from analyze_dictionary_and_compositions import analyze_U_and_A
 from utils import check_file
 import os
+import anndata as ad
 
 
 # Test inputs to compute_dictionary
 class TestPerformanceUandA(unittest.TestCase):
 
     def test_analyze_dictionary_and_composition(self):
-        P = [np.loadtxt(os.path.join('data/compositions_A', 'version_%d.txt' % i),
-                        skiprows=1, usecols=list(range(2, 45))) for i in range(0, 50)]
-        analyze_U_and_A(X_test=np.array(np.load('data/training_data_sce.npy')),
-        U=np.array(np.load('data/gene_modules.npy')),
-        Phi=P,
-        outpath='data')
+        P = [np.loadtxt(os.path.join('data/compositions_A/version_16.txt'),
+                        skiprows=1, usecols=list(range(2, 45)))]
+        results = analyze_U_and_A(X_input=ad.read_h5ad('data/test.h5ad'),
+                                  U=np.array(np.load('data/gene_modules.npy')),
+                                  Phi=P, outpath='data')
+        print(results)
         self.assertTrue(check_file('data/simulation_results.txt', [".txt"]))
 
 
