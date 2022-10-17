1fe0ba9cfe0edf175be0a3eae5789f25e591b1b5
leonorss <leonor.schubert@gmail.com>
2022-10-17 09:37:26 +0200

    Removed unneccesary parameter in code. Added a Snakefile, YAML and python file for the parameter sweep.

---
+++ analysis/parameter_sweep.py
@@ -0,0 +1,40 @@
+## Import libraries
+import numpy as np
+import sys
+
+## Import/Specify path to CISI code
+# Specify CISI code directory path
+CODE_DIR = snakemake.params.cisi_dir
+# Add code directory to systems paths
+sys.path.append(CODE_DIR)
+
+# Import CISI training fnc.
+from train_dictionary_and_compositions import train_U_and_A
+
+
+## Read in snakemake parameters
+k = snakemake.wildcards.k
+d = snakemake.wildcards.d
+nmeasurements = snakemake.wildcards.m
+
+sce = snakemake.params.sce
+test_names = snakemake.params.test_names
+outpath = snakemake.outpath[0]
+default_params = snakemake.params.default_params
+
+
+## Train CISI
+(training_res, training_res_no_noise,
+ U_best, Phi_best, X_test) = train_U_and_A(sce,
+                                           outpath,
+                                           split_by=default_params['split_by'],
+                                           k_cv=default_params['k_cv'],
+                                           test_set=default_params['test_names'],
+                                           lda1=k,
+                                           normalization=default_params['normalization'],
+                                           d=d,
+                                           maxItr=default_params['maxItr'],
+                                           nmeasurements=nmeasurements,
+                                           maxcomposition=default_params['maxcomposition'],
+                                           save=default_params['add_noise'],
+                                           analysis_normalization=default_params['analysis_normalization'])
