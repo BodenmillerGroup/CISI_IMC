28437c6374638752d7c7c40da313a697f0954a97
leonorss <leonor.schubert@gmail.com>
2022-11-02 16:11:31 +0000

    Added config for POC run using best worst protein, added correlation of best run to parameter run output and added python analysis run for fixed A.

--- analysis/tonsil_POC_fixed_A.py
+++ analysis/tonsil_POC_fixed_A.py
@@ -22,30 +22,40 @@ sys.path.append(CODE_DIR)
 
 # Import CISI training fnc.
 from analyze_dictionary_and_compositions import analyze_U_and_A
+from decompress import decompress
+from utils import compare_results
 
 
 ## Specify input paths
 data_path = Path('/mnt/bb_dqbm_volume')
-tonsil_path = Path(os.path.join(data_path,
+training_path = Path(os.path.join(data_path,
                                 'data/Tonsil_th152/preprocessed_data/spe.h5ad'))
-####
 ## TODO: Change!!!
+experiment_path = Path(os.path.join(data_path,
+                                '......'))
+
 U_path = Path(os.path.join(data_path,
-                                'analysis/Tonsil_th152/training/subset/....'))
+                                'analysis/Tonsil_th152/training/subset/proof_of_concept_fixed_A/gene_modules.csv'))
 A_path = Path(os.path.join(data_path,
-                                'analysis/Tonsil_th152/training/subset/....'))
+                                'analysis/Tonsil_th152/training/subset/proof_of_concept_fixed_A/version_1.txt'))
 
 ## Specify output path
-out_path = Path(os.path.join(data_path, 'analysis/Tonsil_th152/training/subset...'))
+out_path_training = Path(os.path.join(data_path, 'analysis/Tonsil_th152/training/subset/proof_of_concept_fixed_A/training'))
+out_path_experiment = Path(os.path.join(data_path, 'analysis/Tonsil_th152/training/subset/proof_of_concept_fixed_A/experiment'))
 # Create output directory if it doesn't exist
-out_path.mkdir(parents=True, exist_ok=True)
-####
+out_path_training.mkdir(parents=True, exist_ok=True)
+out_path_experiment.mkdir(parents=True, exist_ok=True)
+
 
 # Check that input files/dictionary exist
-if not helpers.analysis_utils.is_valid_file(tonsil_path, ['.h5ad']):
+if not helpers.analysis_utils.is_valid_file(training_path, ['.h5ad']):
     # If file is not found, throw error
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
-                            tonsil_path)
+                            training_path)
+if not helpers.analysis_utils.is_valid_file(experiment_path, ['.h5ad']):
+    # If file is not found, throw error
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
+                            experiment_path)    
 if not helpers.analysis_utils.is_valid_file(U_path, ['.csv']):
     # If file is not found, throw error
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
@@ -55,50 +65,105 @@ if not helpers.analysis_utils.is_valid_file(A_path, ['.txt']):
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
                             A_path)
     
-## Specify parameters
-### TODO: change!! 
-dictionary_size = 10
+## Specify parameters for CISI
+dictionary_size = 20
+normalization = 'none'
 
 # Define test rois
-test_names_tonsil = ('20220520_TsH_th152_cisi1_002',)
-
-# Specify k-sparsity of dictionary used in training and name of final folder
-# for each dataset, where results will be saved
-normalization = 'none'
+test_names_training = ('20220520_TsH_th152_cisi1_002',)
 
 
 ## Read in data
 # Read in SingleCellExperiment converted to anndata by cellconverter in R
-sce_tonsil = ad.read_h5ad(tonsil_path)
+sce_training = ad.read_h5ad(training_path)
+sce_experiment = ad.read_h5ad(experiment_path)
 # Channels of interest
-channels_of_interest = ['CD8a', 'CD20', 'CD4', 'CD11c', 'CD3', 'Ki-67', 'SMA', 'panCK']
+proteins_of_interest = ['CD8a', 'CD20', 'CD4', 'CD11c', 'CD3', 'Ki-67', 'SMA', 'panCK']
+channels_of_interest = ['channel 141', 'channel 145', 'channel 148', 'channel 152',
+                       'channel 154', 'channel 165']
 # Remove uninteresting proteins/channels
-sce_tonsil = sce_tonsil[:, sce_tonsil.var.index.isin(channels_of_interest)]
+sce_training = sce_training[:, sce_training.var.index.isin(proteins_of_interest)]
 
 # Read U
-U_tonsil = np.genfromtxt(U_path, delimiter=',', skip_header=True,
+U = np.genfromtxt(U_path, delimiter=',', skip_header=True,
                          usecols=list(range(1, (dictionary_size)+1)))
-U_names = np.genfromtxt(U_path, delimiter=',', usecols=0, skip_header=1,
-                        dtype='S20').tolist()
+U_names = [x.decode() for x in np.genfromtxt(U_path, delimiter=',', usecols=0, skip_header=1,
+                                             dtype='S20')]
 
 # Read A
-A_tonsil = np.loadtxt(A_path, skiprows=1, usecols=list(range(2, len(channels_of_interest)+2))
-A_names = np.loadtxt(A_path, max_rows=1, dtype='S20').tolist()
+A = np.loadtxt(A_path, skiprows=1, usecols=list(range(2, len(proteins_of_interest)+2)))
+A_names = [x.decode() for x in np.loadtxt(A_path, max_rows=1, dtype='S20')]
+A_channels = [x.decode() for x in np.loadtxt(A_path, usecols=1, dtype='S20')]
   
                       
-## Throw error if A and U don't have the same proteins or they are in a different order
-if not U_names!=A_names:
-    # If file is not found, throw error
-    raise ValueError(('A and U do not have the same proteins or they are in a ' +
-                      'different order. Please check both files, to prevent CISI ' +
+## Throw error if A and U don't have the same proteins or A has different channels than the specified channels
+## of interest
+if (all(e in U_names for e in A_names) & all(e in A_names for e in U_names) &
+   all(e in A_channels for e in channels_of_interest) & all(e in channels_of_interest for e in A_channels)):
+    U_index = [U_names.index(ind) for ind in proteins_of_interest]
+    A_index = [A_names.index(ind) for ind in proteins_of_interest]
+    A_channel_index = [A_channels.index(ind) for ind in channels_of_interest]
+    
+    U = U[U_index, :]
+    A = A[A_index, :]
+    A = A[:, A_channel_index]
+    
+else:
+    # Throw error
+    raise ValueError(('A and U do not have the same proteins or A does ' +
+                      'not have the channels of interest.' +
+                      'Please check all files, to prevent CISI ' +
                       'from computing wrong results.\n' +
+                      'Proteins in U: {0}'.format(U_names) +
                       'Proteins in A: {0}\n'.format(A_names) +
-                      'Proteins in U: {0}'.format(U_names))
+                      'Channels in A: {0}\n'.format(A_channels))
                       
 
-## Predict performance of A given A and SCE
-# Test spillover corrected results not spillover corrected data for simulation and analysis
-predicted_res, predicted_res_noisy = analyze_U_and_A(sce_tonsil[sce_tonsil.obs.index.isin(test_names_tonsil), ],
-                                                               U_tonsil, [A_tonsil], ['none'], out_path,
-                                                               None, norm=normalization)
+## Predict performance of A given A,U and SCE
+# Test for training data and real experiment data
+(predicted_res_training, 
+ predicted_res_noisy_training) = analyze_U_and_A(sce_training[sce_training.obs.index.isin(test_names_training), ],
+                                                 U, [A], ['none'], out_path_training,
+                                                 None, norm=normalization)
+(predicted_res_training, 
+ predicted_res_noisy_training) = analyze_U_and_A(sce_experiment[:, sce_experiment.var.index.isin(proteins_of_interest)],
+                                                 U, [A], ['none'], out_path_experiment,
+                                                 None, norm=normalization)
 
+## Decompression
+# Decompress composite channels                     
+decompressed_x = decompress(sce_experiment[:, sce_experiment.var.index.isin(channels_of_interest)], 
+                            U, [A])
+# Remove infinit values                   
+decompressed_x[np.isnan(decompressed_x)] = 0
+# Compute statistics
+decompression_results = compare_results(sce_experiment[:, sce_experiment.var.index.isin(proteins_of_interest)], 
+                                        decompressed_x)
+d_gene = np.percentile(1 - distance.pdist(A.dot(U).T, 'correlation'), 90)
+
+# Open file to save decompression results                 
+decompression_file = open(os.path.join(data_path,
+                                'analysis/Tonsil_th152/training/subset/proof_of_concept_fixed_A/decomposition_simulation_results.txt'), 'w')
+colnames = ['version', 'Overall pearson', 'Overall spearman', 'Gene average',
+            'Sample average', 'Sample dist pearson', 'Sample dist spearman',
+            'Gene dist pearson', 'Gene dist spearman',
+            'Matrix coherence (90th ptile)']
+decompression_file.write('\t'.join(colnames) + '\n')                     
+decompression_file.write('\t'.join([str(x) for x in ['']+decompression_results+[d_gene]]) + '\n')
+
+# Write decomposed X to anndata
+decompressed_anndata = sce_experiment[:, sce_experiment.var.index.isin(proteins_of_interest)].copy()
+for k in list(decompressed_anndata.layers.keys()):
+    del decompressed_anndata.layers[k]
+    decompressed_anndata.X = decompressed_x.T
+    decompressed_anndata.write(os.path.join(path, 'X_decomposed.h5ad'))
+decompression_file.close()
+                 
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
